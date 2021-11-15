2f3c01b8ea2f8d6aa0c0223e6dbf4943eae1ec9f
Cemes99 <luudachanh@gmail.com>
2021-11-15 18:11:10 +0700

    Add Screens, Update App.js

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import Login from './src/screens/Login';
 import Register from './src/screens/Register';
 import Home from './src/screens/Home';
+import ProfileDetails from './src/screens/ProfileDetails';
 
 const Stack = createStackNavigator();
 
@@ -19,6 +20,7 @@ export default function App() {
         <Stack.Screen name="Login" component={ Login } />
         <Stack.Screen name="Register" component={ Register } />
         <Stack.Screen name="Home" component={ Home } />
+        <Stack.Screen name="ProfileDetails" component= {ProfileDetails} />
       </Stack.Navigator>
     </NavigationContainer>
   );
